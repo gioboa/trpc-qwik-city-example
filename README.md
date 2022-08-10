3e04df9f32cbc58d22913b0232808f8a5b942c1f
gioboa <giorgiob.boa@gmail.com>
2022-08-10 13:40:40 +0200

    update dependencies

--- README.md
+++ README.md
@@ -9,3 +9,15 @@ Qwik City application built to show how to use [trpc-qwik-city](https://github.c
 <br />
 
 <img src="./docs/example.png" />
+
+<br />
+
+## Running the application
+
+<br />
+
+To run the app:
+
+```bash
+npm run dev.ssr
+```
--- src/routes/api/trpc/[whatever]/index.ts
+++ src/routes/api/trpc/[whatever]/index.ts
@@ -1,25 +1,13 @@
 import type { EndpointHandler } from "@builder.io/qwik-city";
 import { resolveTRPCResponse } from "trpc-qwik-city";
-import { createContext, router } from "../../../../server/trpc";
+import { router } from "../../../../server/trpc";
 
-export const onGet: EndpointHandler = async ({ request, params }) => {
-  const response = await resolveTRPCResponse({
-    request,
-    params,
-    router,
-    createContext,
-  });
-  const json = await response.json();
-  return json;
+export const onGet: EndpointHandler = async ({ request }) => {
+  const response = await resolveTRPCResponse({ request, router });
+  return await response.json();
 };
 
-export const onPost: EndpointHandler = async ({ request, params }) => {
-  const response = await resolveTRPCResponse({
-    request,
-    params,
-    router,
-    createContext,
-  });
-  const json = await response.json();
-  return json;
+export const onPost: EndpointHandler = async ({ request }) => {
+  const response = await resolveTRPCResponse({ request, router });
+  return await response.json();
 };
